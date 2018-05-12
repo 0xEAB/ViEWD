dfcd8293f74b724afb9e6a1d0a55fb968c509881
0xEAB <desisma@heidel.beer>
2018-05-12 23:21:57 +0200

    Add standalone ViEWD implementation

---
+++ src/viewd.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * ViEWD
+ * A lightweight PHP template engine
+ */
+class ViEWD
+{
+    private $file;
+    private $vars;
+
+    /**
+     * ctor
+     * @param string $file Path of the template file
+     * @param array  $vars Variables accessable from within the template
+     */
+    public function __construct($file, $vars = [])
+    {
+        $this->file = $file;
+        $this->vars = $vars;
+    }
+
+    /**
+     * Binds a variable to the specified template variable,
+     * so it can be access from within the template
+     * @param string $name Name
+     * @param mixed  $var  Variable
+     */
+    public function bind($name, &$var)
+    {
+        $this->vars[$name] =& $var;
+    }
+
+    /**
+     * @returns
+     */
+    function calcPageGenerationTime()
+    {
+        round(microtime(true) - $_SERVER['REQUEST_TIME'], 3);
+    }
+
+    /**
+     * Prints the  ViEWD
+     */
+    public function print()
+    {
+        foreach($this->vars as $key => $value)
+        {
+            $$key = $value;
+        }
+        //$v = $this->vars;
+        include $this->file;
+    }
+
+    /**
+     * Sets the value of the specified template variable
+     * that can be access from within the template
+     * @param string $name Name
+     * @param mixed  $var  Value
+     */
+    public function set($name, $val)
+    {
+        $this->vars[$name] = $val;
+    }
+
+    /**
+     * Note: Implemented using output buffering.
+     */
+    public function __toString()
+    {
+        ob_start();
+        $this->print();
+        return ob_get_clean();
+    }
+}
+
---
+++ src/viewdloader.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * ViEWD
+ * A lightweight PHP template engine
+ */
+class ViEWDLoader
+{
+    const TEMPLATE_DEFAULT_FILE_EXT = '.tpl.php';
+
+    private $fileExt;
+    private $tplDir;
+
+    /**
+     * ctor
+     * @param string $tplDir  Path to the templates directory with trailing `/`
+     * @param string $fileExt File extension
+     */
+    function __construct($tplDir, $fileExt = self::TEMPLATE_DEFAULT_FILE_EXT)
+    {
+        $this->tplDir = $tplDir;
+        $this->fileExt = $fileExt;
+    }
+
+    /**
+     * Loads a new ViEWD with the specified template
+     * @param   string $tplName Name of the template to load
+     * @param   array  $vars    Variables accessable from within the template
+     * @returns ViEWD 
+     */
+    function load($tplName, $vars = [])
+    {
+        return new ViEWD($this->tplDir . $tplName . $this->fileExt, $vars);
+    }
+}
+
