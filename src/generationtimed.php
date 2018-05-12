f5a15e470dc41416d5bdef8721c064e191657bb0
0xEAB <desisma@heidel.beer>
2018-05-12 23:50:40 +0200

    License ViEWD under the terms of the BSL-1.0

--- src/generationtimed.php
+++ src/generationtimed.php
@@ -1,4 +1,8 @@
 <?php
+//          Copyright Elias Batek 2016 - 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
 
 /**
  * GeNERATIONTiMED
